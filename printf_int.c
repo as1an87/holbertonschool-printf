310ecf363562e5908ba399c45b6f42258cf10e42
as1anof <as1anoff87@gmail.com>
2024-03-27 11:53:31 +0000

    print int func

---
+++ printf_int.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+ * printf_int - print int
+ * @ptr: pointer to the va_list containing the string
+ * @len: pointer to the length counter
+ *
+ */
+
+void printf_int(va_list ptr, int *len)
+{
+	unsigned int i = 1;
+	int n = va_arg(ptr, int);
+
+	if (n < 0)
+	{
+		n = -n;
+		(*len)++;
+		_putchar('-');
+	}
+	while ((n / i) / 10 != 0)
+	{
+		i = i * 10;
+		(*len)++;
+	}
+	for (; i != 1; i /= 10)
+	{
+		_putchar((number / i) + '0');
+
+		n = n % i;
+	}
+	(*len)++;
+	_putchar((n % 10) + '0');
+}
+
